704f020552b9e2c9e4c14e7920861804cfb1aab0
Edward Armstrong <edward.parry.armstrong@gmail.com>
2017-08-11 17:28:07 +1200

    Setting file import correct

--- chch_streetart/settings.py
+++ chch_streetart/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from settings_secret import *
+from .settings_secret import *
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
