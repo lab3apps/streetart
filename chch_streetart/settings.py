64d1dbf9e9508d07296ab0e7765b09172fb636ba
Chris Marffy <marffyc@gmail.com>
2017-08-14 10:23:16 +1200

    merge conflict, and update to gitignore

--- chch_streetart/settings.py
+++ chch_streetart/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 from .settings_secret import *
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
---
+++ streetart/settings_secret.py
@@ -0,0 +1,16 @@
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'ux-f+(_)oprjw^n#*%(ljk_9$*l+xtwu891$ygnl-@3mb%h=_8'
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+DATABASES = {
+     "default": {
+         "ENGINE": "django.db.backends.postgresql_psycopg2",
+         "NAME": "WTSAdmin",
+         "USER": "WTSAdmin",
+         "PASSWORD": "6BNtVJNQ8vda",
+         "HOST": "localhost",
+         "PORT": "",
+     }
+ }
