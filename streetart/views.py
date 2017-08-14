8230f838c57a5d4e22ca4b22a49ecfd6f4bbf9ab
Chris Marffy <marffyc@gmail.com>
2017-08-14 16:52:28 +1200

    implemented a login page for the website, and integrated twitter and facebook social authentication

--- streetart/views.py
+++ streetart/views.py
@@ -1,12 +1,70 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.views import generic
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AdminPasswordChangeForm, PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.contrib import messages
+from social_django.models import UserSocialAuth
 
 from .models import Question
 
-class IndexView(generic.ListView):
-    template_name = 'streetart/index.html'
+
+@login_required
+def home(request):
+    return render(request, 'streetart/home.html')
+
+@login_required
+def settings(request):
+    user = request.user
+
+    try:
+        github_login = user.social_auth.get(provider='github')
+    except UserSocialAuth.DoesNotExist:
+        github_login = None
+
+    try:
+        twitter_login = user.social_auth.get(provider='twitter')
+    except UserSocialAuth.DoesNotExist:
+        twitter_login = None
+
+    try:
+        facebook_login = user.social_auth.get(provider='facebook')
+    except UserSocialAuth.DoesNotExist:
+        facebook_login = None
+
+    can_disconnect = (user.social_auth.count() > 1 or user.has_usable_password())
+
+    return render(request, 'registration/settings.html', {
+        'twitter_login': twitter_login,
+        'facebook_login': facebook_login,
+        'can_disconnect': can_disconnect
+    })
+
+@login_required
+def password(request):
+    if request.user.has_usable_password():
+        PasswordForm = PasswordChangeForm
+    else:
+        PasswordForm = AdminPasswordChangeForm
+
+    if request.method == 'POST':
+        form = PasswordForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordForm(request.user)
+    return render(request, 'registration/password.html', {'form': form})
+
+
+class HomeView(generic.ListView):
+    template_name = 'streetart/home.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
@@ -42,4 +100,3 @@ def vote(request, question_id):
         return HttpResponseRedirect(reverse('streetart:results', args=(question.id,)))
 
 
-        
