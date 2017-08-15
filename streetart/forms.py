e94a1af93a325c77271fa7c4e794044e04307a3b
Chris Marffy <marffyc@gmail.com>
2017-08-15 16:36:51 +1200

    implementing header bar and further authentication options

---
+++ streetart/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class SignUpForm(UserCreationForm):
+	birth_date = forms.DateField(required=False, help_text='Optional. Format: YYYY-MM-DD')
+	first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+	last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+	email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+	class Meta:
+		model = User
+		fields = ('username', 'first_name', 'last_name', 'email', 'birth_date', 'password1', 'password2')
