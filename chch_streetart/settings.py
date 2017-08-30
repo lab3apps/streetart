4f1404dbc46cb0ac9c75851b99fa16069f3cb4ba
Chris Marffy <marffyc@gmail.com>
2017-08-30 21:55:13 +1200

    squashing migrations and changing forms

--- chch_streetart/settings.py
+++ chch_streetart/settings.py
@@ -69,6 +69,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'django_comments',
     'adminsortable2',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
--- streetart/forms.py
+++ streetart/forms.py
@@ -9,6 +9,38 @@ from django_select2.forms import (
     ModelSelect2TagWidget,
 )
 from django.utils.encoding import force_text
+from mapwidgets.settings import MapWidgetSettings, mw_settings
+
+
+def minify_if_not_debug(asset):
+    """
+        Transform template string `asset` by inserting '.min' if DEBUG=False
+    """
+    return asset.format("" if not mw_settings.MINIFED else ".min")
+
+class MyGooglePointFieldWidget(GooglePointFieldWidget):
+    template_name = "streetart/google-point-field-widget.html"
+    @property
+    def media(self):
+        css = {
+            "all": [
+                minify_if_not_debug("mapwidgets/css/map_widgets{}.css"),
+            ]
+        }
+
+        if not mw_settings.MINIFED:  # pragma: no cover
+            js = [
+                "mapwidgets/js/jquery_class.js",
+                "mapwidgets/js/django_mw_base.js",
+                "mapwidgets/js/mw_google_point_field.js",
+            ]
+        else:
+            js = [
+                "mapwidgets/js/mw_google_point_field.min.js"
+            ]
+
+        return forms.Media(js=js, css=css)
+        
 
 class SignUpForm(UserCreationForm):
     birth_date = forms.DateField(required=False, help_text='Optional. Format: DD/MM/YYYY', widget=forms.DateInput(attrs={'class': 'form-control datepicker', 'name': 'birth_date', 'type': 'date'}))
@@ -54,16 +86,9 @@ def is_int(s):
 class ArtworkForm(forms.ModelForm):
     class Meta:
         model = Artwork
-        fields = ('title', 'artists', 'image', 'photo_credit', 'location')
+        fields = ('submitter_description', 'submitter_name', 'submitter_email', 'image', 'location')
         widgets = {
-            'location': GooglePointFieldWidget,
-            'artists': ArtistSelect2TagWidget(
-                attrs={
-                    "data-width": "100%",
-                    "data-placeholder": "Search Artists",
-                    'data-token-separators': [','],
-                }
-            ),
+            'location': MyGooglePointFieldWidget,
         }
 
 class MuralCommissionForm(forms.ModelForm):
@@ -74,7 +99,10 @@ class MuralCommissionForm(forms.ModelForm):
 class WallSpaceForm(forms.ModelForm):
     class Meta:
         model = WallSpace
-        fields = ('title', 'description', 'contact')
+        fields = '__all__'
+        widgets = {
+            'wall_location': MyGooglePointFieldWidget,
+        }
 
 class ArtistExpressionOfInterestForm(forms.ModelForm):
     class Meta:
