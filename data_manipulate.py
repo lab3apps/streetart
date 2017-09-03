9319f99d216f19cb8be455b66d96b3bcd30eb690
Edward Armstrong <edward.parry.armstrong@gmail.com>
2017-09-03 13:28:19 +1200

    Fixes for slug

--- data_manipulate.py
+++ data_manipulate.py
@@ -3,11 +3,13 @@ from streetart.models import Artwork, Artist, Artwork_Category, Crew, Status, Ro
 from django.template.defaultfilters import slugify
 
 
-def change_artwork_slug:
+def change_artwork_slug():
 	all_artworks = Artwork.objects.all()
 	for artwork in all_artworks:
 		if artwork.slug == '' or artwork.slug == None:
-			artwork.slug = slugify(artwork.pk)
+			slug_ = slugify(artwork.pk)
+			print(slug_)
+			artwork.slug = slug_
 			artwork.save()
 
 change_artwork_slug()
