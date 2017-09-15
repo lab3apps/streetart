5274567cb2ab9a9b6f018d1a2a4cadbec6d3a7d9
Lab3 Admin <admin@lab3.co.nz>
2017-09-15 06:42:49 +0000

    Changes from merge

--- streetart/middleware/remote_addr_middleware.py
+++ streetart/middleware/remote_addr_middleware.py
@@ -1,16 +1,7 @@
-from django.http import HttpResponse
+from django.utils.deprecation import MiddlewareMixin
 
-class RemoteAddrMiddleware(object):
-	def __init__(self, get_response):
-		self.get_response = get_response
-
-	def __call__(self, request):
-		return self.get_response(request)
-
-	def process_exception(self, request, exception): 
-		return HttpResponse("in exception")
-
+class RemoteAddrMiddleware(MiddlewareMixin):
 	def process_request(self, request):
-		if 'HTTP_X_FORWARDED_FOR' in request.META:
-			ip = request.META['HTTP_X_FORWARDED_FOR'].split(',')[0].strip()
-			request.META['REMOTE_ADDR'] = ip
+		if  'HTTP_X_FORWARDED_FOR' in request.META:
+			request.META['REMOTE_ADDR'] = request.META['HTTP_X_FORWARDED_FOR'].split(",")[0].strip()
+		return None
