f1da8f877b88086fed04b4ea52433405e659b160
Edward Armstrong <edward.parry.armstrong@gmail.com>
2017-09-15 18:13:13 +1200

    Removing sorl thumbnails. Blog should be working. Now bio is showing on artworks

--- streetart/static/streetart/js/map.js
+++ streetart/static/streetart/js/map.js
@@ -156,12 +156,34 @@ function focusOnMarker(index) {
         //Used to get focused artworks id for liking and checking in.
         $('#main-card').data('index', index);
         var artists_text = '';
+        var artists_bio_html = '';
+        console.log(art.artists);
         for(var key in art.artists) {
             if (arrayHasOwnIndex(art.artists, key)) {
                 if (artists_text != '') {
                     artists_text += ', ';
                 }
-                artists_text += art.artists[key];
+                artists_text += art.artists[key]['name'];
+                artists_bio_html += '<br><div>';
+                if (art.artists[key]['name'] && art.artists[key]['name'] != '' && art.artists[key]['name'] != 'None'){
+                    artists_bio_html += '<span>'+art.artists[key]['name']+'</span><br>';
+                }
+                if (art.artists[key]['website'] && art.artists[key]['website'] != '' && art.artists[key]['website'] != 'None'){
+                    artists_bio_html += '<span><a href="'+art.artists[key]['website']+'">'+art.artists[key]['website']+'</a> </span><br>';
+                }
+                if (art.artists[key]['facebook'] && art.artists[key]['facebook'] != '' &&  art.artists[key]['facebook'] != 'None'){
+                    artists_bio_html += '<span>FB: <a href="https://www.facebook.com/'+art.artists[key]['facebook']+'">'+art.artists[key]['facebook']+'</a> </span><br>';
+                }
+                if (art.artists[key]['instagram'] && art.artists[key]['instagram'] != '' && art.artists[key]['instagram'] != 'None'){
+                    artists_bio_html += '<a href="https://www.instagram.com/'+art.artists[key]['instagram']+'">@'+art.artists[key]['instagram']+'</a> </span><br>';
+                }
+                if (art.artists[key]['twitter'] && art.artists[key]['twitter'] != '' && art.artists[key]['twitter'] != 'None'){
+                    artists_bio_html += '<span><a href="https://www.twitter.com/'+art.artists[key]['twitter']+'">'+art.artists[key]['twitter']+'</a> </span><br>';
+                }
+                if (art.artists[key]['biography'] && art.artists[key]['biography'] != '' && art.artists[key]['biography'] != 'None'){
+                    artists_bio_html += '<span>'+art.artists[key]['biography']+'</a> </span><br>';
+                }
+                artists_bio_html += '</div>';
             }
         }
         console.dir(art);
@@ -193,6 +215,7 @@ function focusOnMarker(index) {
                 <span class="artwork-description">'+art.description+'</span>\
             </p>');
         }
+
         for(var key in art) {
             if (art[key] != 'None' && art[key] != '') {
                 if (key == 'commission_date' || key == 'decommission_date') {
@@ -208,6 +231,7 @@ function focusOnMarker(index) {
                 }
             }
         }
+        $("#card-content").append(artists_bio_html);
         $('.overlay-fullscreen').attr('href', art.imageUrl);
         if(art.hasLiked === 'True') {
             $('.like i').html('favorite');
@@ -297,7 +321,7 @@ function filterMarkers() {
                 var artist_found = false;
                 for(var artist_key in art.artists) {
                     if (arrayHasOwnIndex(art.artists, artist_key)) {
-                        if (art.artists[artist_key].toLowerCase().indexOf(searchText.toLowerCase()) >= 0 &&
+                        if (art.artists[artist_key]['name'].toLowerCase().indexOf(searchText.toLowerCase()) >= 0 &&
                             catArray.indexOf(art.status) >= 0) {
                             artist_found = true;
                             marker.setVisible(true);
