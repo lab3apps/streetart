1a1a3393b58f49d60e625ea11218f87d7522cf95
Chris Marffy <marffyc@gmail.com>
2017-09-19 17:53:48 +1200

    updating settings

--- chch_streetart/settings.py
+++ chch_streetart/settings.py
@@ -207,3 +207,22 @@ COMMENTS_XTD_APP_MODEL_OPTIONS = {
 }
 
 CITY = 'Christchurch'
+
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
+    'fields': 'id,name,email', 
+}
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.mail.mail_validation',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details'
+)
